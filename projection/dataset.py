2c1675b3ab2f3f3b7cfbbe211d0d046811d80c7f
Joyce <objdoctor891213a@gmail.com>
2022-04-15 16:11:40 +0800

    update tensorflow version

--- projection/dataset.py
+++ projection/dataset.py
@@ -1,25 +1,27 @@
 import math
 import random
+import numpy as np
+import tensorflow as tf
 
-import torch
-from torch.utils.data import Dataset, DataLoader
 
-
-class Projection(Dataset):
+class ProjectionDataset(tf.keras.utils.Sequence):
     # Constant for the gravity
     g = 9.81
 
-    def __init__(self, max_theta, max_velocity, batch_size):
+    def __init__(self, max_theta=90, max_velocity=100, batch_size=4096):
         self.max_theta = max_theta
         self.max_velocity = max_velocity
         self.max_height = (max_velocity**2) / (2 * self.g)
         self.max_x_range = (max_velocity**2) / self.g
         self.batch_size = batch_size
 
-    def __len__(self):
-        return self.batch_size
+    def on_epoch_end(self):
+        pass
 
-    def __getitem__(self, idx):
+    def __len__(self):
+        return 1
+
+    def __generate_data(self):
         # You don't need idx in fact, just randomly generate the data
         theta = random.random() * self.max_theta
         velocity = random.random() * self.max_velocity
@@ -27,17 +29,27 @@ class Projection(Dataset):
         # Compute the real answer
         height = (velocity * math.sin(math.radians(theta)))**2 / (2 * self.g)
         x_range = (velocity**2) * math.sin(math.radians(theta * 2)) / self.g
-        height = torch.tensor([height / self.max_height])
-        x_range = torch.tensor([x_range / self.max_x_range])
-
-        combine = torch.cat([
-            torch.tensor([theta / self.max_theta]),
-            torch.tensor([velocity / self.max_velocity])
+        data = np.array([
+            height / self.max_height,
+            x_range / self.max_x_range
+        ])
+        combine = np.array([
+            theta / self.max_theta,
+            velocity / self.max_velocity
         ])
 
-        return combine, (height, x_range)
+        return data, combine
 
+    def __generate_batch(self):
+        data_list, combine_list = [], []
 
-def make_loader(max_theta=90.0, max_velocity=100, batch_size=128):
-    dataset = Projection(max_theta=max_theta, max_velocity=max_velocity, batch_size=batch_size)
-    return DataLoader(dataset, batch_size=batch_size)
+        for _ in range(self.batch_size):
+            data, combine = self.__generate_data()
+            data_list.append(data)
+            combine_list.append(combine)
+
+        return np.array(combine_list), np.array(data_list)
+
+    def __getitem__(self, idx):
+        return self.__generate_batch()
+
